07fa1895f4ca71068e0ff9323fd298b875f90a31
Mr.4L1F <39854087+aliferik02@users.noreply.github.com>
2018-09-09 21:31:44 +0700

    Add files via upload

---
+++ 4L1F.sh
@@ -0,0 +1,679 @@
+#!/bin/bash
+#Mau Ngapain Cuk
+#Recode nggak Bakal Jadiin Lu mastah
+#mau ganti Author Ya ijin ke gw dulu
+#mikir pakai otak
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+echo $green
+echo "Mr.4L1F./";
+sleep 0.1;
+clear
+echo $purple
+echo 'Mr.4L1F..-';
+sleep 0.1;
+clear
+echo $cyan
+echo 'Mr.4L1F...\';
+sleep 0.1;
+clear
+echo $red
+echo "Mr.4L1F..../";
+sleep 0.1;
+clear
+echo $blue
+echo 'Mr.4L1F.....-';
+sleep 0.1;
+clear
+echo $green
+echo 'Mr.4L1F......\';
+sleep 0.1;
+clear
+echo $purple
+echo "Mr.4L1F......./";
+sleep 0.1;
+clear
+echo $cyan
+echo "Mr.4L1F........-";
+sleep 0.1;
+clear
+echo $red
+echo 'Mr.4L1F.........\';
+sleep 0.1;
+clear
+echo $blue
+echo 'Mr.4L1F..........-';
+sleep 0.1;
+clear
+echo $green
+echo "Mr.4L1F.........../";
+sleep 0.1;
+clear
+echo $purple
+echo 'Mr.4L1F............-';
+sleep 0.1;
+clear
+echo $cyan
+echo 'Mr.4L1F.............\';
+sleep 0.1;
+clear
+echo $red
+echo "Mr.4L1F............../";
+sleep 0.1;
+clear
+echo $blue
+echo "Mr.4L1F.............../";
+sleep 0.1;
+clear
+echo $green
+echo 'Mr.4L1F................-';
+sleep 0.1;
+clear
+echo $purple
+echo 'Mr.4L1F.................\';
+sleep 0.1;
+clear
+echo $cyan
+echo "Mr.4L1F................../";
+sleep 0.1;
+clear
+echo $red
+echo 'Mr.4L1F...................-';
+sleep 0.1;
+clear
+echo $blue
+echo 'Mr.4L1F....................\';
+sleep 0.1;
+clear
+echo $green
+echo "Mr.4L1F...................../";
+sleep 0.1;
+clear
+echo $purple
+echo "Mr.4L1F......................-";
+sleep 0.1;
+clear
+echo $cyan
+echo 'Mr.4L1F.......................\';
+sleep 0.1;
+clear
+echo $red
+echo 'Mr.4L1F........................-';
+sleep 0.1;
+clear
+echo $blue
+echo 'Mr.4L1F........................./';
+sleep 0.1;
+clear
+echo $green
+echo 'Mr.4L1F..........................-';
+sleep 0.1;
+clear
+echo $purple
+echo 'Mr.4L1F...........................\';
+sleep 0.1;
+clear
+echo $cyan
+echo 'Mr.4L1F............................-';
+sleep 0.1;
+clear
+echo $red
+echo 'Mr.4L1F............................/';
+sleep 0.1;
+clear
+echo $blue
+echo 'Mr.4L1F.............................-';
+sleep 0.1;
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo $yellow'==============================================='
+echo $red'[4L1F]====== SELAMAT MENGGUNAKAN  ======[4L1F]'
+echo $red'[4L1F]===== Code by Mr.4L1F =====[4L1F]'
+echo $white'[4L1F]=== BELUM PUNYA TEAM ===[4L1F]'
+echo $white'[4L1F]== Kontak Me : 08981380315 ==[4L1F]'
+echo $yellow'==============================================='
+echo $white'##############  WARNING TOOLS  ##############'
+echo $yellow'==============================================='
+echo $red' [1] Scan Directory';
+echo $red' [2] Bruteforce Directory';
+echo $red' [3] XAttacker';
+echo $red' [4] Mgenerator';
+echo $red' [5] Striker';
+echo $white' [6] blackeye';
+echo $white' [7] Cr3dOv3r';
+echo $white' [8] Web Application Audit Framework';
+echo $white' [9] Hash Code';
+echo $white' [10] server domains';
+echo $red' [11] Apple Valid Mail Checker';
+echo $red' [12] Exploit CMS Wordpress'
+echo $red' [13] CSV scanner';
+echo $red' [14] Drupal RCE';
+echo $red' [15] CMS scanner';
+echo $white' [16] Joomscan';
+echo $white' [17] Sqlmap';
+echo $white' [18] The Fat Rat';
+echo $white' [19] Fsociety';
+echo $white' [20] Mailer cli';
+echo $red' [21] ipv4 Bypass';
+echo $red' [22] Email Spammer';
+echo $red' [23] Spammer Bot';
+echo $red' [24] XSS-Radar';
+echo $red' [25] XSS Hunter';
+echo $white' [26] XSS Hunter Client';
+echo $white' [27] domxssscanner';
+echo $white' [28] xsser';
+echo $white' [29] Brute XSS';
+echo $white' [30] XSStrike';
+echo $red' [31] ssrfDetector';
+echo $red' [32] ground-control';
+echo $red' [33] tplmap';
+echo $red' [34] LFISuite';
+echo $red' [35] getsploit';
+echo $white' [36] Findsploit';
+echo $white' [37] retire.js';
+echo $white' [38] jwt_tool';
+echo $white' [39] Metasploit';
+echo $white' [40] viSQL';
+echo $yellow'================================================'
+echo $red' [0] Exit';
+echo $yellow'================================================'
+read -p "r00t@Mr.4L1F~$" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f standard -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install dirhunt
+echo $red 'jika ingin menggunakan dirhunt ketik : dirhunt [target web kamu]'
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f standard -F gay "Mr.4L1F"
+pkg upgrade
+pkg install python2
+pkg install git
+pkg clone https://github.com/stanislav-web/OpenDoor.git
+mv OpenDoor $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+toilet -f standard -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install perl
+pkg install git
+git clone https://github.com/Moham3dRiahi/XAttacker/
+mv XAttacker $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet -f standard -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/m4rktn/mgenerator
+mv mgenerator $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f standard -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/UltimateHackers/Striker
+mv Striker $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f standard -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/thelinuxchoice/blackeye
+mv blackeye $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet -f standard -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/D4Vinci/Cr3dOv3r.git
+mv Cr3dOv3r $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/m4ll0k/Galileo
+mv Galileo $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/haijuga7/Hashcode
+mv Hashcode $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/cyweb/server_domains
+mv server_domains $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/HarisTanone/Apple-Email-Valid
+mv server_domains $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/Moham3dRiahi/WP-Grab-Info
+mv WP-Grab-Info $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/radenvodka/SVScanner
+mv SVScanner $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install wget
+wget https://pastebin.com/raw/M0cCyaB7 -O rce.sh
+chmod +x rce.sh
+mv rce.sh $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/Dionach/CMSmap.git
+mv CMSmap $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/rezasp/joomscan.git
+mv joomscan $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/Screetsec/TheFatRat.git
+mv TheFatRat $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/Manisso/fsociety
+mv fsociety $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 20 ] || [ $bro = 20 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/pedro-stanaka/mailer-cli.git
+mv mailer-cli $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 21 ] || [ $bro = 21 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/milo2012/ipv4Bypass
+mv ipv4Bypass $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 22 ] || [ $bro = 22 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/Juniorn1003/Email-Spammer
+mv Email-Spammer $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 23  ] || [ $bro = 23 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/3pehrdev/spammer-bot
+mv spammer-bot  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 24  ] || [ $bro = 24 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/bugbountyforum/XSS-Radar
+mv XSS-Radar  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 25  ] || [ $bro = 25 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/mandatoryprogrammer/xsshunter
+mv xsshunter  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 26  ] || [ $bro = 26 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/mandatoryprogrammer/xsshunter_client
+mv xsshunter_client  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 27  ] || [ $bro = 27 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/yaph/domxssscanner
+mv domxssscanner  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 28  ] || [ $bro = 28 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/epsylon/xsser
+mv xsser  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 29  ] || [ $bro = 29 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/rajeshmajumdar/BruteXSS
+mv BruteXSS  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 30  ] || [ $bro = 30 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/s0md3v/XSStrike
+mv XSStrike  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 31  ] || [ $bro = 31 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/JacobReynolds/ssrfDetector
+mv ssrfDetector  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 32  ] || [ $bro = 32 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/jobertabma/ground-control
+mv ground-control  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 33  ] || [ $bro = 33 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/epinna/tplmap
+mv tplmap  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 34  ] || [ $bro = 34 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/D35m0nd142/LFISuite
+mv LFISuite  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 35  ] || [ $bro = 35 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/vulnersCom/getsploit
+mv getsploit  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 36  ] || [ $bro = 36 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/1N3/Findsploit
+mv Findsploit  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 37  ] || [ $bro = 37 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/RetireJS/retire.js
+mv retire.js  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 38  ] || [ $bro = 38 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/ticarpi/jwt_tool
+mv jwt_tool  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 39  ] || [ $bro = 39 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/rapid7/metasploit-framework
+mv metasploit-framework  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 40  ] || [ $bro = 40 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg inatall git
+git clone https://github.com/blackvkng/viSQL
+mv viSQL  $HOME
+echo $red 'done install'
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo $red 'Selamat Berkarya'
+exit
+fi
